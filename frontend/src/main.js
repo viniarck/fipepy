e8ea5180343a2b40c89421d4f7fc332556cb8fae
Vinicius Arcanjo <viniarck@gmail.com>
2019-04-14 09:49:20 -0300

    Added vuetify materialize CSS dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vSelect from 'vue-select'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 Vue.component('v-select', vSelect)
+Vue.use(Vuetify)
 
 new Vue({
   render: h => h(App),
